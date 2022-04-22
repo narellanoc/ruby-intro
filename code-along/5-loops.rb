0e0cdfae285638b2502b73d7a8765dd1221cb376
narellanoc <nicolas.arellano@kellogg.northwestern.edu>
2022-04-22 21:29:03 +0000

    listo

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -7,5 +7,42 @@
 #   puts "tacos!"
 # end
 
+# loop do
+#   puts "tacos!"
+#   break
+# end
+
+# counter = 0
+# loop do
+#     if counter == 5
+#         break
+#     end
+#     puts "tacos!"
+#     counter = counter + 1
+# end
+
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+
+# index = 0
+# loop do
+#     if index == tacos.count
+#         break
+#     end
+#     puts tacos[index]
+#     index = index + 1
+# end
+
+# index = 0
+# loop do
+#     if index == tacos.count
+#         break
+#     end
+#     taco = tacos[index]
+#     puts taco
+#     index = index + 1
+# end
+
+for taco in tacos
+    puts taco
+end
