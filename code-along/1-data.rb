7b87bbcfa8de72db6ad48f19357b5c970b415648
narellanoc <nicolas.arellano@kellogg.northwestern.edu>
2022-04-22 19:02:00 +0000

    1

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -2,18 +2,37 @@
 # is the same as where this file is located and then run:
 # ruby 1-data.rb
 
+# puts "Hello world"
+
 # In Ruby, there are different *types* of data:
 
 # Numbers
+# puts 7.77
 
 # Perform simple math with numbers
+# puts 5+2
 
 # Strings
+# puts "tacos"
+#ruby is forgiving
+# puts "tacos" *3 
+# this does not work: 
+# puts "tacos" +3
 
 # Combine strings together
+# puts "3 tacos"
+# puts "#{3} tacos"
+# puts "#{3+3} tacos"
 
 # Variables
+quantinty = 3 + 3 + 3
+food = "tacos"
 
 # Combine strings and variables
+# puts quantinty + food  ->error
+# puts "#{quantinty} #{food}"
 
 # String manipulation
+# puts "Hello".upcase
+# puts "Hello".reverse
+# puts "Hello".swapcase
