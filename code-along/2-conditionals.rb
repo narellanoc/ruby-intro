4cb6cdcc125e3e9afd413553e883cdd408dafbf0
narellanoc <nicolas.arellano@kellogg.northwestern.edu>
2022-04-22 19:27:51 +0000

    2

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,59 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+# this_is_true = true
+# puts this_is_true
+# this_is_false = false
+# puts this_is_false
 
 # Boolean Expressions
+# puts 3 == 2
+# puts 3!=2
+# puts 3>2
+# puts 3<2
 
 # If Conditional Logic
+# if 3 == 2
+#     #do something
+#     puts "hello"
+# end
+
+# if 3 > 2
+#     #do something
+#     puts "hello"
+# end
+
+user_entered_password = "tacos"
+real_password = "secret"
+
+# if user_entered_password == real_password
+#     puts "You are in"
+# end
+
+# if user_entered_password != real_password
+#     puts "You are in"
+# end
+
 
 # If/Else Conditional Logic
 
 # Elsif Conditional Logic
+# your_team_score = 3
+# other_team_score = 3
+
+# if your_team_score > other_team_score
+#     puts "You Win"
+# elsif your_team_score == other_team_score
+#     puts "you tied"
+# else
+#     puts "you lose"
+# end
 
 # Combining Expressions
+# temp = 68
+# precip = 0
+
+# if temp >= 65 && temp <= 90
+#     puts "it's nice outside"
+# end
+
